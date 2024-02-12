e1bd788eb9359b0bbc7bbb641bb11a05fe005358
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-02-12 18:58:39 +0530

    Time: 32 ms (15.01%) | Memory: 45.8 MB (75.36%) - LeetSync

---
+++ DP/503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,73 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+int n=nums.length;
+    // Stack<Integer> s=new Stack<>();
+    int[] ans=new int[n];
+    int fg;  
+    for(int i=n-1;i>=0;i--)
+    {  fg=0;
+       for(int j=i+1;j<n;j++)
+       {
+           if(nums[i]<nums[j])
+            {
+                ans[i]=nums[j];
+                fg=1;
+                break;
+            }
+       }
+
+    //    System.out.println("fg:"+fg);
+         if(fg==0)
+         {
+             for(int k=0;k<n-(n-i);k++)
+             {
+                 if(nums[i]<nums[k])
+                 {
+                     ans[i]=nums[k];
+                     fg=1;
+                     break;
+                 }
+             }
+         }
+
+        // System.out.println(Arrays.toString(ans));
+
+         if(fg==0)
+           ans[i]=-1;    
+
+    }
+
+
+return ans;
+
+        }
+}
+         
+       
+// //            Output: [2,-1,2]
+
+// //            [1,2,3,4,3]
+// //            Output: [2,3,4,-1,4]
+
+       
+//         int n = nums.length;
+//         int[] ans = new int[nums.length];
+//         Stack<Integer> stk = new Stack<>();
+//         ans[n-1] = -1;
+//         stk.push(nums[n-1]);
+//         for (int i =  2*n-1 ;i>= 0; i--) {
+//             if (stk.peek() > nums[i%n])
+//                 ans[i%2] = stk.peek();
+//             while (!stk.isEmpty() && stk.peek() <= nums[i%n])
+//                 stk.pop();
+//             if (stk.isEmpty()) {
+//                 ans[i%n] = -1;
+//             } else ans[i%n] = stk.peek();
+//             stk.push(nums[i%n]);
+//         }
+//         for (int i : ans)
+//             System.out.print(i + " ");
+//             return ans;
+//     }
+// }
+
