6cd0ca5ab54293144277aba1c7219a039d09c6a1
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-02-13 19:13:44 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (60.90%) - LeetSync

---
+++ DP/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class Solution {
+    public int minAddToMakeValid(String s) {
+      int n=s.length();
+      int count=0;
+       int  st = 0;
+       for(char ch:s.toCharArray())
+       {
+         if( st==0 && ch==')' )
+            count++;
+         else if(st>0 && ch==')')
+             st--;
+         else st++;
+
+       }
+       
+       return count+st;
+    }
+}
+//        int count=0;
+//         int stack=0;
+//         for(int i=0;i<n;i++)
+//         {    
+//             ch=s.charAt(i);
+
+//             if(ch==')' && stack==0)
+//             { count++;
+//               continue;  
+//             }
+//             else if(ch=='(')
+//               stack++;
+//             else if(ch==')')
+//                stack--;  
+
+
+//         }
+
+//         while(stack!=0)
+//         {
+//             stack--;
+//             count++;
+//         }
+
+//         return count;
+
+
+        
+//     }
+// }
+
+
+// // "(()(((((()()))()()))("
+
+
+
+
+// // (
+// // (       
+// // (
+// //   = 3
+
+
+
+
+// // https://certificate.codingninjas.com/coding_recap/033f239bc8050d43
