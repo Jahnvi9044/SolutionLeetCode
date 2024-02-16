dae2f7b1ea62cdf53c63fa0a62cc86b39e06c0ae
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-02-16 16:52:48 +0530

    Time: 42 ms (86.04%) | Memory: 63.8 MB (13.85%) - LeetSync

--- DP/1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
+++ DP/1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
@@ -4,32 +4,20 @@ class Solution {
        HashMap<Integer,Integer> map=new HashMap<>();
        for(int a:arr) map.put(a,map.getOrDefault(a,0)+1);
        
-       Pair[] pair=new Pair[map.size()];
-       
-        int p=0;
+      
 
-       for( Map.Entry<Integer,Integer> e:map.entrySet())
-       {
-           int a=e.getKey();
-           int b=e.getValue();
-           pair[p]=new Pair(a,b);
-           p++;
-       }
-        Comparator<Pair> obj=(o1,o2)->(o1.b).compareTo(o2.b);
-       Arrays.sort(pair,obj);
+        ArrayList<Integer> arr1=new ArrayList<>(map.values());
+        Collections.sort(arr1);
       int count=0;
         for(int i=0;i<map.size();i++)
        {
-           
-      
-       int bb= pair[i].b;
+         int bb= arr1.get(i);
 
          if(bb<=k)
         {   
             count++;
-            int c=k-bb;
-            pair[i].b=-1;
-            k=c;
+            k=k-bb;
+           
         }
          
          else {
@@ -40,32 +28,23 @@ class Solution {
            if(k==0)
              break;
        }
-
-    //    for(int i=0;i<pair.length;i++)
-    //    {  
-    //        System.out.println(pair[i].print());
-    //    }
-      
-       return pair.length-count;
+       return arr1.size()-count;
    
     }
 }
 
 
-class Pair
-{
-   Integer a;
-   Integer b;
+// class Pair
+// {
+//    Integer a;
+//    Integer b;
 
-   Pair(Integer a,Integer b)
-   {
-       this.a=a;
-       this.b=b;
+//    Pair(Integer a,Integer b)
+//    {
+//        this.a=a;
+//        this.b=b;
 
-   }
+//    }
 
-   public String print()
-   {
-       return this.a+" "+this.b;
-   }
-}
+  
+// }
