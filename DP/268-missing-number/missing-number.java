a4b3700f8585bac316c461bb37b665c237567704
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-02-20 21:32:36 +0530

    Time: 2 ms (33.38%) | Memory: 45.4 MB (25.42%) - LeetSync

--- DP/268-missing-number/missing-number.java
+++ DP/268-missing-number/missing-number.java
@@ -1,9 +1,7 @@
 class Solution {
     public int missingNumber(int[] nums) {
        int n=nums.length;
-    //    [3,0,1]
-    //    [0,1,2]
-    //    [-1,-1,1]  
+
 
     int xor=0;
          for(int i = 0 ; i<n ; i++ )
