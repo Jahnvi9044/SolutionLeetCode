c9c5839fb79592bed1af7bf2e8e34bbc45ca0ac6
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-02-25 07:55:04 +0530

    Time: 51 ms (76.34%) | Memory: 49 MB (47.15%) - LeetSync

--- DP/2761-array-reduce-transformation/array-reduce-transformation.js
+++ DP/2761-array-reduce-transformation/array-reduce-transformation.js
@@ -8,8 +8,8 @@ var reduce = function(nums, fn, init) {
    var  a=init;
     for(let i=0;i<nums.length;i++)
     {
-        a=fn(init, nums[i]);
-        init=a;
+        a=fn(a, nums[i]);
+        
     }
     return a;
 };
