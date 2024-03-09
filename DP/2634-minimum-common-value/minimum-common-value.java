301b229258a19cf4a999fb316744afd256912ae4
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-03-09 21:37:20 +0530

    Time: 0 ms (100.00%) | Memory: 62.4 MB (80.15%) - LeetSync

--- DP/2634-minimum-common-value/minimum-common-value.java
+++ DP/2634-minimum-common-value/minimum-common-value.java
@@ -1,6 +1,11 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
+          
+          
+          
           int i=0,j=0;
+         if( nums1[nums1.length - 1 ] < nums2[0]  ||  nums2[nums2.length - 1 ] < nums1[0] )
+           return -1;
 
          while( i < nums1.length && j < nums2.length )
          {
