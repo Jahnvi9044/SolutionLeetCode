a471d7dcc172f91789d512a25a23e2ba54d4e1ea
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-06-12 16:48:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (60.77%) - LeetSync

--- DP/75-sort-colors/sort-colors.java
+++ DP/75-sort-colors/sort-colors.java
@@ -1,28 +1,32 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int i=0,j=0,k=nums.length-1,temp=0;
-        while(i<=k)
-        {
-               if(nums[i]==0)
-               {
-                    temp=nums[i];
-                    nums[i]=nums[j];
-                    nums[j]=temp;
-                    j++;
-                    i++;
+        
+      int i = 0, j = 0, k= nums.length - 1 ;
 
-               }
-               else if(nums[i]==1)
-               {
-                    i++;
-               }
-               else if(nums[i]==2)
-               {    temp=nums[i];
-                    nums[i]=nums[k];
-                    nums[k]=temp;
-                    k--;
-                   
-               }
+      while(i<=k)
+      {
+        if(nums[i]==0)
+        {
+           nums[i]=nums[j];
+           nums[j]=0;
+           j++;
+           i++;
+          
         }
+        else if(nums[i]==2)
+        {
+           nums[i]=nums[k];
+           nums[k]=2;
+           k--;
+           
+        }
+        else if(nums[i]==1){
+        i++;
+        }
+      } 
+
+
+
+
     }
 }
