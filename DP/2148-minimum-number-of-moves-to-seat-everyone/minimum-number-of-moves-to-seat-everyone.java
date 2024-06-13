8a23085d9a6ee51983fb4841fc0ce0fb7d66cc2d
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-06-13 19:45:37 +0530

    Time: 3 ms (62.42%) | Memory: 44.4 MB (9.02%) - LeetSync

---
+++ DP/2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+         
+     Arrays.sort(seats);
+     Arrays.sort(students);
+      int  n =seats.length;
+      int count = 0;
+     for(int i =0 ;i<n;i++)
+     {  
+        count += Math.abs(seats[i]-students[i]); 
+
+     }
+
+     return count ; 
+
+    }
+}
