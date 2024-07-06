f33a480475b512d3e102c015f0775e46653354d6
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-07-06 21:16:04 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (46.33%) - LeetSync

--- DP/2645-pass-the-pillow/pass-the-pillow.java
+++ DP/2645-pass-the-pillow/pass-the-pillow.java
@@ -1,38 +1,14 @@
 class Solution {
     public int passThePillow(int n, int time) {
-          
-          int person = 1 ;
 
-    
 
-        //   st.push(1);   time = 10
-        //  1->2->3->4    p = 3 , time = i = 10
-          for(int i = 0 ; i<time ;)
-          {    
-               while(person<n && i<time )
-               { if(time-i>n-1)
-                {
-                person+=(n-1);
-                i+=(n-1);
-                continue;
-                } 
-                person++;
-                 i++;
-               }
-               while(person>1 && i<time)
-               { 
-                if(time-i>n-1)
-                {
-                    person-=(n-1);
-                    i+=(n-1);
-                    continue;
-                }
-                person--;
-                i++;
-               }
-          } 
-          return person;
+         int div = n - 1;
+         int afterDivide = time/div;
+         int off = div * afterDivide ;
 
+          if(afterDivide % 2 == 0)
+          return 1 + (time-off);
+          else return n-(time-off); 
 
     }
 }
