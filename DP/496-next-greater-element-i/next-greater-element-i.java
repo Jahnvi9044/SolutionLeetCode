0d9b53da3a37599431a9ba031cdaca193dc22dd6
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-08-06 16:24:59 +0530

    Time: 15 ms (5.12%) | Memory: 44.5 MB (10.12%) - LeetSync

--- DP/496-next-greater-element-i/next-greater-element-i.java
+++ DP/496-next-greater-element-i/next-greater-element-i.java
@@ -1,84 +1,45 @@
 class Solution {
-
-    HashMap<Integer,Integer> map=new HashMap<>();
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-     
-     int n1=nums1.length;
-     int n2=nums2.length;
-     
-    
-     NGR(nums2);
-      int[] ans=new int[n1];
-     for(int i=0;i<n1;i++)
+     int n1 =nums1.length;
+     int n2 =nums2.length;   
+
+     int[] freq = new int[10001];
+     Stack<Integer>  s = new Stack<>();
+     for(int i = n2-1; i>=0 ; i--)
      {
-          if(map.containsKey(nums1[i]))
-              {
-                  ans[i]= map.get(nums1[i]);
-                 
-              }
         
+                while(!s.isEmpty() && nums2[i]>=s.peek())
+                {
+                    s.pop();
+                }
+
+                if(s.isEmpty())
+                {
+                    freq[nums2[i]]=-1;
+                }
+
+                else if(nums2[i]<s.peek())
+                {
+                    freq[nums2[i]]=s.peek();
+                }
+
+            
+
+            s.push(nums2[i]);   
+
      }
 
- return ans;
-     }
+     System.out.print(Arrays.toString(freq));
+    
 
-    public void NGR(int[] nums)
+    int [] ans = new int[nums1.length];
+
+    for(int  i =0 ;i<nums1.length;i++)
     {
-        int n=nums.length;
-        int pseudo=-1;
-        Stack<Integer> s=new Stack<>();
-        for(int i=n-1;i>=0;i--)
-        {
-            while(!s.isEmpty() && s.peek()<=nums[i] )
-              s.pop();
-            if(s.isEmpty())
-              map.put(nums[i],-1);
-            else 
-              map.put(nums[i],s.peek());
-            s.push(nums[i]);    
-        }
+        ans[i] = freq[nums1[i]];
+
+    }
+
+    return ans ;
     }
 }
-
-
-//        int n=nums2.length;
-//         int[] ans = new int[n];
-        
-//         Stack<Integer> stk = new Stack<>();
-//         ans[n-1] = -1;
-//         stk.push(nums2[n-1]);
-//         for (int i = n-2; i >= 0; i--)
-//          {
-       
-//             while (!stk.isEmpty() && stk.peek() < nums2[i])
-//                 stk.pop();
-//             if (stk.isEmpty()) 
-//                  ans[i] = -1;
-           
-//             else ans[i] = stk.peek();
-
-//              stk.push(nums2[i]);
-//         }
-         
-//          int[] ans2=new int[nums1.length];
-      
-
-//       HashMap<Integer,Integer> map=new HashMap<>();
-//        for(int j=0;j<nums2.length;j++)
-//              {
-//                     map.put(nums2[j],ans[j]);
-                
-//              }
-
-//          for(int i=0;i<nums1.length;i++)
-//          {
-//                  if(map.containsKey(nums1[i]))
-//                  {
-//                       ans2[i]=map.get(nums1[i]);
-                      
-//                  } 
-             
-//          }
-//             return ans2;
-//     }
-// }
