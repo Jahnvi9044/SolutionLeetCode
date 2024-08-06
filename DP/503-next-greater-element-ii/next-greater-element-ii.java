a81c9eb75fd6d918937ff434bbb11a64221a9531
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-08-06 17:29:50 +0530

    Time: 305 ms (5.02%) | Memory: 46 MB (69.23%) - LeetSync

--- DP/503-next-greater-element-ii/next-greater-element-ii.java
+++ DP/503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,102 +1,38 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-    int n=nums.length;
-    Stack<Integer> s=new Stack<>();
-    int[] ans=new int[n];
-    
-
-    //just think there are 2 arrays like this
-    for(int i=2*n-1;i>=0;i--)
-    {
-        while(!s.isEmpty() && nums[i%n]>=s.peek())
+        
+      Stack<Integer> s = new Stack<>();
+      int n = nums.length;
+      for(int i = n-1 ; i>=0 ; i--)
+      {  
+        System.out.println(nums[i]);
+        while(!s.isEmpty() && s.peek()<=nums[i])
           s.pop();
-        if(s.isEmpty())
-          ans[i%n]=-1;
-        else if(nums[i%n]<s.peek())
-          ans[i%n]=s.peek();
+        s.push(nums[i]);
+          
+          
+      }
+      System.out.println(s);
+       int [] ans =  new int[nums.length];
 
-         s.push(nums[i%n]);   
-    }
+        for(int i = n-1 ; i>=0 ; i--)
+      {
 
-    return ans;
+        System.out.println(nums[i]);
+        while(!s.isEmpty() && s.peek()<=nums[i])
+          s.pop();
+
+        if(s.isEmpty()) 
+           ans[i]=-1;
+
+        else if(s.peek()>nums[i])     
+            ans[i]=s.peek();
+         s.push(nums[i]);
+          
+          
+      }
+
+      return ans;
 
     }
 }
-
-
-
-
-
-
-
-
-
-
-//     int fg;  
-//     for(int i=n-1;i>=0;i--)
-//     {  fg=0;
-//        for(int j=i+1;j<n;j++)
-//        {
-//            if(nums[i]<nums[j])
-//             {
-//                 ans[i]=nums[j];
-//                 fg=1;
-//                 break;
-//             }
-//        }
-
-//     //    System.out.println("fg:"+fg);
-//          if(fg==0)
-//          {
-//              for(int k=0;k<n-(n-i);k++)
-//              {
-//                  if(nums[i]<nums[k])
-//                  {
-//                      ans[i]=nums[k];
-//                      fg=1;
-//                      break;
-//                  }
-//              }
-//          }
-
-//         // System.out.println(Arrays.toString(ans));
-
-//          if(fg==0)
-//            ans[i]=-1;    
-
-//     }
-
-
-// return ans;
-
-//         }
-// }
-         
-       
-// //            Output: [2,-1,2]
-
-// //            [1,2,3,4,3]
-// //            Output: [2,3,4,-1,4]
-
-       
-//         int n = nums.length;
-//         int[] ans = new int[nums.length];
-//         Stack<Integer> stk = new Stack<>();
-//         ans[n-1] = -1;
-//         stk.push(nums[n-1]);
-//         for (int i =  2*n-1 ;i>= 0; i--) {
-//             if (stk.peek() > nums[i%n])
-//                 ans[i%2] = stk.peek();
-//             while (!stk.isEmpty() && stk.peek() <= nums[i%n])
-//                 stk.pop();
-//             if (stk.isEmpty()) {
-//                 ans[i%n] = -1;
-//             } else ans[i%n] = stk.peek();
-//             stk.push(nums[i%n]);
-//         }
-//         for (int i : ans)
-//             System.out.print(i + " ");
-//             return ans;
-//     }
-// }
-
