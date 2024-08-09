c2ae891d22daa899c0dd1ecbf8a016e6f0790f7b
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-08-09 18:57:16 +0530

    Time: 73 ms (6.76%) | Memory: 47.8 MB (8.53%) - LeetSync

--- DP/2340-total-appeal-of-a-string/total-appeal-of-a-string.java
+++ DP/2340-total-appeal-of-a-string/total-appeal-of-a-string.java
@@ -1,49 +1,46 @@
 class Solution {
     public long appealSum(String s) {
 
-        HashMap<Character,List<Integer>> map = new HashMap<>();
+     
         long  n = s.length(); 
-        for( int i = 0 ; i<s.length() ; i++)
-        {
-           if(!map.containsKey(s.charAt(i)))
+        long no = n*(n+1)/2;
+        long sum = 0l ;
+
+        for(char ch = 'a';ch<='z';ch++)  
+        {  
+            List<Integer> list = new ArrayList<>();
+            list.add(-1);
+           for( int i = 0 ; i<s.length() ; i++)
            {
-              map.put(s.charAt(i),new ArrayList<>());
-              map.get(s.charAt(i)).add(-1);
+               if(s.charAt(i)==ch)
+              {
+                 list.add(i);
              
-           }
-         
-           map.get(s.charAt(i)).add(i);
-          
-        }
-      
-        for(List<Integer> e :map.values()  )
-        {
-           e.add(s.length());
-        }
-         
-          long no = n*(n+1)/2;
-          long sum = 0l ;
-         
-         for (Map.Entry<Character, List<Integer>> entry : map.entrySet())
-         {
-
-            long sub = 0l;
-            long contri= 0l ;
-            List<Integer> list = entry.getValue();
-            for(int i = 0;i<list.size()-1;i++)
-            {
-
-              long   su = list.get(i+1)-list.get(i)-1;
-                su = su*(su+1)/2;
-                sub += su ;  
-
+              }
+        
             }
+            list.add(s.length()); 
+
+
+             long sub = 0l;
+             long contri= 0l ;
+
+              for(int i = 0;i<list.size()-1;i++)
+             {
+
+                long   su = list.get(i+1)-list.get(i)-1;
+                 su = su*(su+1)/2;
+                 sub += su ;  
+
+             }
 
             contri = no - sub ; 
             sum += contri;
             
-         } 
-            System.out.println(sum);
+
+
+        }
+            // System.out.println(sum);
 
         return sum ;
     }
