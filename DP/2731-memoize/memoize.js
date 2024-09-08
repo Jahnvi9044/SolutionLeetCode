bd57f919a3ad0a8328355e9ceaff230901b209f2
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-09-08 18:58:42 +0530

    Time: 317 ms (29.14%) | Memory: 89 MB (66.57%) - LeetSync

---
+++ DP/2731-memoize/memoize.js
@@ -0,0 +1,52 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+     
+    
+    let map = new Map();
+    let callCount = 0 ;   
+
+
+    // Map.set() , Map.get() , Map.delete() , Map.has
+   
+
+    return function(...args) 
+    { 
+    
+    let ans = 0;
+    let st = JSON.stringify(args);
+         if(map.has(st))
+         {
+              ans = map.get(st)
+         }
+         else 
+         { 
+            callCount++ ; 
+            ans = fn(...args);
+            map.set(st,ans);
+                
+
+         }
+       
+        //  console.log(map);
+         return ans ; 
+
+       }
+       
+         
+    
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
