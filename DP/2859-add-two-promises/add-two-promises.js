cce46ea5f194e69b47054fcd13aecae2703e624e
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-09-08 19:48:06 +0530

    Time: 46 ms (99.16%) | Memory: 49.6 MB (40.69%) - LeetSync

---
+++ DP/2859-add-two-promises/add-two-promises.js
@@ -0,0 +1,33 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+
+  let ans = 0 ;
+   ans += await promise1;
+   ans += await promise2;
+  console.log(ans );
+  const pr = new Promise((response, reject)=>{
+  
+    response(ans ) 
+
+  }); 
+
+
+return pr ; 
+  
+
+
+
+};
+
+
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
+
+
