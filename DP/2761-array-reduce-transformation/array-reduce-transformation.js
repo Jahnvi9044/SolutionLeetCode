96d6f4aa09fcc972c438650373509962ae3c3a46
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-09-08 14:33:07 +0530

    Time: 46 ms (91.19%) | Memory: 49 MB (82.48%) - LeetSync

--- DP/2761-array-reduce-transformation/array-reduce-transformation.js
+++ DP/2761-array-reduce-transformation/array-reduce-transformation.js
@@ -5,6 +5,26 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
+  
+
+    nums.forEach((item)=>{
+        let val  = fn(init,item);
+        init = val ;
+
+    });  
+    return init ; 
+};
+
+
+
+
+/*
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ 
+var reduce = function(nums, fn, init) {
    var  a=init;
     for(let i=0;i<nums.length;i++)
     {
@@ -13,3 +33,4 @@ var reduce = function(nums, fn, init) {
     }
     return a;
 };
+*/
