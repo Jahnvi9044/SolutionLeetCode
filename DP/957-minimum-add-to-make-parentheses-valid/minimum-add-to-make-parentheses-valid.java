d23f77eed920ed9bee3b4cafadc4fb2d6c8a5d3f
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-10-10 04:09:25 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (28.61%) - LeetSync

--- DP/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ DP/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,66 +1,143 @@
-import java.util.*;
+
 
 class Solution {
     public int minAddToMakeValid(String s) {
-      int n=s.length();
-      int count=0;
-       int  st = 0;
-       for(char ch:s.toCharArray())
-       {
-         if( st==0 && ch==')' )
-            count++;
-         else if(st>0 && ch==')')
-             st--;
-         else st++;
+        
 
-       }
-       
-       return count+st;
+      int stack = 0;
+      int count = 0;
+         int i = 0;
+         int n = s.length(); 
+      while(i<n)
+      {
+        char ch = s.charAt(i);
+
+        if(ch == '(' )
+          stack++;
+        else if(ch==')' && stack!=0)
+           stack--;
+        else if(ch==')' && stack==0)
+           count++; 
+
+           i++;    
+      }
+
+     count +=  stack;
+     
+     return count;
     }
 }
-//        int count=0;
-//         int stack=0;
-//         for(int i=0;i<n;i++)
-//         {    
-//             ch=s.charAt(i);
-
-//             if(ch==')' && stack==0)
-//             { count++;
-//               continue;  
-//             }
-//             else if(ch=='(')
-//               stack++;
-//             else if(ch==')')
-//                stack--;  
 
 
-//         }
 
-//         while(stack!=0)
-//         {
-//             stack--;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import java.util.*;
+
+// class Solution {
+//     public int minAddToMakeValid(String s) {
+//       int n=s.length();
+//       int count=0;
+//        int  st = 0;
+//        for(char ch:s.toCharArray())
+//        {
+//          if( st==0 && ch==')' )
 //             count++;
-//         }
+//          else if(st>0 && ch==')')
+//              st--;
+//          else st++;
 
-//         return count;
+//        }
+       
+//        return count+st;
+//     }
+// }
+// //        int count=0;
+// //         int stack=0;
+// //         for(int i=0;i<n;i++)
+// //         {    
+// //             ch=s.charAt(i);
+
+// //             if(ch==')' && stack==0)
+// //             { count++;
+// //               continue;  
+// //             }
+// //             else if(ch=='(')
+// //               stack++;
+// //             else if(ch==')')
+// //                stack--;  
+
+
+// //         }
+
+// //         while(stack!=0)
+// //         {
+// //             stack--;
+// //             count++;
+// //         }
+
+// //         return count;
 
 
         
-//     }
-// }
+// //     }
+// // }
 
 
-// // "(()(((((()()))()()))("
-
-
-
-
-// // (
-// // (       
-// // (
-// //   = 3
+// // // "(()(((((()()))()()))("
 
 
 
 
-// // https://certificate.codingninjas.com/coding_recap/033f239bc8050d43
+// // // (
+// // // (       
+// // // (
+// // //   = 3
+
+
+
+
+// // // https://certificate.codingninjas.com/coding_recap/033f239bc8050d43
