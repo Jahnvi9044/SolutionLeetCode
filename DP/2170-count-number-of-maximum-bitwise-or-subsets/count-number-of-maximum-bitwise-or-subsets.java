69297bb5a5988c48737c5f8677ecb92aa4a489ef
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-10-18 14:50:38 +0530

    Time: 9 ms (67.71%) | Memory: 40.7 MB (85.96%) - LeetSync

--- DP/2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ DP/2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,38 +1,35 @@
 class Solution {
+    int max = 0 ;
+    int freq = 0;
     public int countMaxOrSubsets(int[] nums) {
-        List<Integer> arr = new ArrayList<>();
+        
 
-        subsets(arr,0,0,nums,nums.length);
-         int max = 0;
-        for(Integer i : arr )
-        {  
-            max = (max<i)? i : max;
-            
-        }
-        int count = 0;
-        for(Integer i : arr)
-        {
-            if(max == i )
-                count++;
-        }
-        return count ; 
+        subsets(0,0,nums,nums.length);
+        return freq ; 
     }
 
-    public void subsets(List<Integer> arr ,int val , int i, int[] nums,int n )
+    public void subsets(int val , int i, int[] nums,int n )
     {
         if(n==i)
-        {
-            arr.add(val);
+        {   if(max<val)
+            {   max=val;
+                freq=0;
+            }
+            if(max==val)
+            {
+                freq++;
+            }
+         
             return ;           
         }
 
         //take i th element of nums 
         
-        subsets(arr,val|nums[i],i+1,nums,n);
+        subsets(val|nums[i],i+1,nums,n);
 
         // do not take i th element of nums
       
-        subsets(arr,val,i+1,nums,n);  
+        subsets(val,i+1,nums,n);  
 
     }
 }
