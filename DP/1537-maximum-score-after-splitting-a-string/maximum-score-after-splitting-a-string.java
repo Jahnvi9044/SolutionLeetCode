251ae342330cda4494024862174eb64050ff2d05
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2025-01-01 21:56:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (25.32%) - LeetSync

--- DP/1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ DP/1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -2,41 +2,24 @@ class Solution {
     public int maxScore(String s) {
         
      int n =s.length();
-     int arr0[] = new int[n];
-     int k = 0 ;
-     
-     for(int i = 0 ; i<n ; i++ )
-     {
-        if(s.charAt(i)=='0')
-          {
-            arr0[i] = k + 1; 
-            k=arr0[i];
-          }
-          else arr0[i] = k;
-     }
- 
-
-    int [] arr1=new int[n];
-      k = 0;
-
-      for(int i = n-1 ; i>=0 ; i-- )
+    
+     int max = -501 ;
+     int one=0,zero=0;
+     for(int i = 0 ; i<n-1 ; i++ )
      {
         if(s.charAt(i)=='1')
           {
-            arr1[i] = k + 1; 
-            k = arr1[i];
+           one++;
           }
-          else arr1[i] = k;
-     }
-
-    int max = 0 ;
-    for(int i = 0;i<n-1;i++)
-    {
-        int ans = arr0[i] + arr1[i+1];
+          else zero++;
+  
+        int ans= zero-one;
         max = (max>(ans))?max:ans;
     }
-    //  System.out.println(Arrays.toString(arr0));
-    //  System.out.println(Arrays.toString(arr1));     
-      return max;
+
+    one+= (s.charAt(n-1)=='1')?1:0;
+
+      
+      return one + max;
     }
 }
