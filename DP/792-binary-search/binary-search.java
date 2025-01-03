82189dd2427ac24ec05a7102626ec14f57ff5037
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2025-01-03 09:23:00 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (37.70%) - LeetSync

---
+++ DP/792-binary-search/binary-search.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+
+        int low = 0,high = nums.length-1;
+          int mid = 0; 
+        while(low<=high)
+        {
+
+            mid = low + (high -low )/2;
+            if(nums[mid]==target)
+             return mid;
+            else if (nums[mid]>target)
+              high = mid -1 ;
+            else 
+              low = mid+1 ;
+
+        }
+
+        return -1 ;
+
+    }
+}
