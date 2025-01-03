0c2ee0f8f05bccfe934ab61c79f7d2e694052954
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2025-01-03 13:19:08 +0530

    Time: 7 ms (10.60%) | Memory: 43 MB (97.43%) - LeetSync

--- DP/90-subsets-ii/subsets-ii.java
+++ DP/90-subsets-ii/subsets-ii.java
@@ -1,37 +1,35 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
 
-        ArrayList<Integer> ds = new ArrayList<Integer>();
-            
-       int n= nums.length ;
-       List<List<Integer>> ans = new ArrayList<>();
+      ArrayList<Integer> arr = new ArrayList<Integer>();
 
-        Arrays.sort(nums);
-        subset( nums , 0 , ds, n , ans   );    
-       
-       return ans ;
-        
+      List<List<Integer>> ans = new ArrayList<>();
+      Arrays.sort(nums);
+      SubsetWithDuplicate(nums,0,nums.length,arr,ans);
+      
+      return ans ;
+
     }
 
 
-   public void  subset(int[] nums,int i ,ArrayList<Integer> ds, int n , List<List<Integer>> ans )
-   {
-      if( i == n )
-      { 
-        if(!ans.contains(ds))
-        { 
-             ans.add(new ArrayList<Integer>(ds));
-        }
-        return ;
-      }
-     //pick
-     ds.add(nums[i]);
-     subset(nums,i+1,ds,n,ans);
-     //un-pick 
-     ds.remove((Integer)nums[i]);
-     subset(nums,i+1,ds,n,ans);
-    
-
-   }
-    
+    public void SubsetWithDuplicate(int [] nums , int i , int n, ArrayList<Integer> arr ,List<List<Integer>> ans )
+    {
+         if(i==n)
+         {
+            if(!ans.contains(arr))
+            {
+                ans.addFirst(new ArrayList<Integer>(arr));
+            }
+            return ;
+         }
+         
+         //take
+         arr.add(nums[i]);
+         SubsetWithDuplicate(nums,i+1,n,arr,ans);   
+ 
+         //not take
+         arr.remove(arr.size()-1);
+         SubsetWithDuplicate(nums,i+1,n,arr,ans);
+         return ;
+    }
 }
