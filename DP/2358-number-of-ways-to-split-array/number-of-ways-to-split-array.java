334adc2402f2b9954826b4ba01f4cd525edac8de
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2025-01-03 09:17:34 +0530

    Time: 3 ms (59.69%) | Memory: 56.6 MB (98.80%) - LeetSync

--- DP/2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
+++ DP/2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
@@ -3,25 +3,26 @@ class Solution {
        
        int n = nums.length;
 
-    long []pre =  new long[n];  
-       long sum = 0;
+      long pre = 0l;
+       long sum = 0l;
         for(int i = 0;i<n;i++)
         { 
-           sum += nums[n-i-1];
-           pre[n-i-1] = sum ;
+           pre += nums[n-i-1];
+
         }
          sum=0;
-        //  System.out.println(Arrays.toString(pre));
+         pre-=nums[0];
        int count =0 ; 
        for(int i = 0 ;i<n-1 ;i++)
        {
            sum += nums[i] ; 
               
-        //    System.out.println(sum+" "+pre[i+1]);
-           if(sum>=pre[i+1])
+        
+           if(sum>=pre)
            {
              count++;
            } 
+           pre-=nums[i+1];
        }
        
        return count ; 
