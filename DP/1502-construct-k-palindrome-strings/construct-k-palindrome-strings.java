f4ed91f52aba3695962748ce21b925306ef00630
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2025-01-11 12:39:09 +0530

    Time: 4 ms (94.60%) | Memory: 45.4 MB (45.32%) - LeetSync

--- DP/1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
+++ DP/1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -3,17 +3,14 @@ class Solution {
         
      
       int n = s.length();
-       if(n<k) 
-          return false ;
+       if(n<k) return false ;
+       if(n==k) return true;
+
       int odd = 0,count=0 ;
       for(int i = 0 ; i<n ; i++)
       {   
             char ch = s.charAt(i);
-             
-          
             odd ^= 1 << (ch - 'a');
-        
-           
       }
       
        
