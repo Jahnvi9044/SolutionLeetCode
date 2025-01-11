c08d7f366062d24521603e2ec3d9be1754a34741
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2025-01-11 12:37:28 +0530

    Time: 4 ms (94.60%) | Memory: 45.7 MB (19.42%) - LeetSync

--- DP/1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
+++ DP/1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -1,24 +1,25 @@
 class Solution {
     public boolean canConstruct(String s, int k) {
         
-      int[] freq = new int[26] ;
+     
       int n = s.length();
+       if(n<k) 
+          return false ;
       int odd = 0,count=0 ;
       for(int i = 0 ; i<n ; i++)
       {   
             char ch = s.charAt(i);
-             freq[ch-'a']++;
-             if(freq[ch-'a']%2==0)
-              odd--;
-             else 
-               odd++;  
-             count++;  
+             
+          
+            odd ^= 1 << (ch - 'a');
+        
+           
       }
+      
        
-       if(odd>k)
+       if(Integer.bitCount(odd)>k)
          return false;
-        if(count<k) 
-          return false ;
+       
   
        return true;
 
