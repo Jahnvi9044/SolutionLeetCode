9fa8c4ceec77bd143aa005f951fd2850df7dced8
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2025-01-14 11:00:06 +0530

    Time: 81 ms (40.67%) | Memory: 50.9 MB (8.67%) - LeetSync

--- DP/3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
+++ DP/3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
@@ -1,22 +1,26 @@
 class Solution {
     public int minimumLength(String s) {
-        int [] freq = new int[26];
+        HashMap<Character,Integer> map = new HashMap<>();
+
 
         for(Character ch :s.toCharArray())
         {
-            freq[ch-'a']++;
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        
-        int count = 0;
-        for(int i = 0;i<26;i++)
-        {
-            if(freq[i]<=2) count+=freq[i];
 
-            else if(freq[i]%2==0) count+=2;
+        // Iterator it = map.iterator();
+        int count = 0;
+        
+        for(Integer i : map.values())
+        {
+             if(i<=2) count+=i;
+
+            else if(i%2==0) count+=2;
             else count+=1;
         }
 
-        return count; 
+        
+        return count;
 
     }
 }
