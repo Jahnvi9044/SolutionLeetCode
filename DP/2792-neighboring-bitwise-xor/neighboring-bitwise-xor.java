ed30502737eaaae84d71596764c72e7194bc9a17
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2025-01-17 23:58:24 +0530

    Time: 9 ms (18.24%) | Memory: 62.4 MB (49.41%) - LeetSync

--- DP/2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
+++ DP/2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
@@ -1,6 +1,11 @@
 class Solution {
     public boolean doesValidArrayExist(int[] derived) {
+        
+        if(derived.length == 1 && derived[0]==0)
+          return true ; 
+
         int countone = 0 ;
+
        for(int i : derived)
        {
         if(i == 1 )
