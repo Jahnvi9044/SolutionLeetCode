3f2373cf074de525f7147e48671f86efc40878fa
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2025-05-03 05:46:35 +0530

    Time: 31 ms (5.74%) | Memory: 46.7 MB (99.13%) - LeetSync

---
+++ DP/1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+       int max = Integer.MIN_VALUE;
+       int n = weights.length;
+       int sum = 0;
+       for(int i :weights)
+       {
+         sum+=i;
+         max = Math.max(max,i);
+       }
+       int low = max,high = sum,mid ;
+     System.out.println(find_tf(15,weights,days));
+       while(low<=high)
+       { 
+        mid = low + (high-low)/2;
+          System.out.println(low+" "+high);
+        if(find_tf(mid,weights,days))
+        {
+            high = mid-1;
+            System.out.println(mid);
+        }
+        else low = mid+1;
+       }
+       return low ;
+
+    }
+
+    public boolean find_tf(int mid, int[] weights , int days)
+    {    int n =weights.length;
+         int sum = 0,count=0;
+        for(int i = 0;i<n;i++)
+        {
+            if(sum<mid)
+            {
+                sum+=weights[i];
+            }
+            if(sum>=mid) 
+            {  count++;
+                if(sum==mid)
+                {
+                    
+                     sum=0;
+                }
+                else
+                {         
+                     sum=weights[i];   
+                }  
+                // sum -=mid;
+            }
+            
+        }
+        if(sum!=0)
+        {
+            count++;
+        }
+        System.out.println("count="+count);
+        return days>=count;
+    }
+}
