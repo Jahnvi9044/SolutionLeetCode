ef5354d6fbe0fe6b1ea5481cd41ccdfc4c9cf153
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2025-07-16 13:12:27 +0530

    Time: 1219 ms (43.67%) | Memory: 0B (100.00%) - LeetSync

--- DP/1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ DP/1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-select unique_id, name from Employees left outer join EmployeeUNI on (Employees.id = EmployeeUNI.id);
+
+Select unique_id , name from Employees e1 left Join EmployeeUNI e2 on e1.id = e2.id ; 
+
